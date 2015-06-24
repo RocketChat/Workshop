17585c4c7bb48fbc5ac9022c573dcdfdb7314814
Gabriel Engel <gabriel.engel@fgsys.com>
2015-06-24 18:48:15 -0300

    SHOWING MESSAGES

--- client/views/channel/channel.js
+++ client/views/channel/channel.js
@@ -1,4 +1,9 @@
 Template.channel.helpers({
+  messages: function() {
+    var _id = Router.current().params._id;
+    return Messages.find({_channel: _id});
+  },
+
   channel: function() {
     var _id = Router.current().params._id;
     return Channels.findOne({_id: _id});
@@ -10,6 +15,8 @@ Template.channel.events({
     if (event.keyCode == 13 && !event.shiftKey) { // Check if enter was pressed (but without shift).
       var _id = Router.current().params._id;
       var value = instance.find('textarea').value;
+      // Markdown requires double spaces at the end of the line to force line-breaks.
+      value = value.replace("\n", "  \n");
       instance.find('textarea').value = ''; // Clear the textarea.
       Messages.insert({_channel: _id, message: value});
     }
